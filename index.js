5d2bc3f6943057ebff187d836fb020c8cdad81ed
swishypants <shamuflute@gmail.com>
2015-12-01 21:13:01 -0500

    changed color and font

--- index.js
+++ index.js
@@ -28,13 +28,30 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+	response.render('pages/index');
 });
 
 app.get('/about', function(request, response) {
-  response.render('pages/about');
+	response.render('pages/about');
+});
+
+app.get('/find', function(request, response) {
+	response.render('pages/find');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+function generate_random() {
+	console.log("Button clicked");
+	/*connection.query("SELECT * FROM Dog ORDER BY RANDOM() LIMIT 0,1", function(err, rows) {
+		console.log("Connection opened");
+		if(err) throw err;
+		//connection.end()
+		return {r:rows};
+		connection.end();
+		console.log("Connection closed");
+	});*/
+	document.getElementById("random_dog").innerHTML = "Hello World";
+}
