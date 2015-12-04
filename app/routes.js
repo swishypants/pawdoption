c18a23ffc1708682f46f4419be9c7421e583795c
swishypants <shamuflute@gmail.com>
2015-12-04 18:14:48 -0500

    prints all dogs

---
+++ app/routes.js
@@ -0,0 +1,172 @@
+module.exports = function(app, passport) {
+	
+	var LocalStrategy = require('passport-local').Strategy;
+
+	// =========================================================================
+    // passport session setup ==================================================
+    // =========================================================================
+    // required for persistent login sessions
+    // passport needs ability to serialize and unserialize users out of session
+
+    // used to serialize the user for the session
+    passport.serializeUser(function(user, done) {
+		done(null, user.id);
+    });
+
+    // used to deserialize the user
+    passport.deserializeUser(function(id, done) {
+		connection.query("select * from users where id = "+id,function(err,rows){	
+			done(err, rows[0]);
+		});
+    });
+	
+
+ 	// =========================================================================
+    // LOCAL SIGNUP ============================================================
+    // =========================================================================
+    // we are using named strategies since we have one for login and one for signup
+	// by default, if there was no name, it would just be called 'local'
+
+    passport.use('local-signup', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'username',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, username, password, done) {
+
+		// find a user whose email is the same as the forms email
+		// we are checking to see if the user trying to login already exists
+        connection.query("select * from users where username = '" + username + "'", function(err,rows){
+			console.log(rows);
+			console.log("above row object");
+			if (err)
+                return done(err);
+			 if (rows.length) {
+                return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
+            } else {
+
+				// if there is no user with that email
+                // create the user
+                var newUserMysql = new Object();
+				
+				newUserMysql.username = username;
+                newUserMysql.password = password; // use the generateHash function in our user model
+			
+				var insertQuery = "INSERT INTO users ( username, password ) values ('" + username +"','"+ password +"')";
+					console.log(insertQuery);
+				connection.query(insertQuery,function(err,rows){
+				newUserMysql.id = rows.insertId;
+				
+				return done(null, newUserMysql);
+				});	
+            }	
+		});
+    }));
+	
+	// =========================================================================
+    // LOCAL LOGIN =============================================================
+    // =========================================================================
+    // we are using named strategies since we have one for login and one for signup
+    // by default, if there was no name, it would just be called 'local'
+
+    passport.use('local-login', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'username',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, username, password, done) { // callback with email and password from our form
+
+        // find a user whose email is the same as the forms email
+        // we are checking to see if the user trying to login already exists
+        User.findOne({ 'local.username' :  username }, function(err, user) {
+            // if there are any errors, return the error before anything else
+            if (err)
+                return done(err);
+
+            // if no user is found, return the message
+            if (!user)
+                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+
+            // if the user is found but the password is wrong
+            if (!user.validPassword(password))
+                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+
+            // all is well, return successful user
+            return done(null, user);
+        });
+
+    }));
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+// normal routes ===============================================================
+
+    // show the home page (will also have our login links)
+    /*app.get('/', function(req, res) {
+        res.render('index.ejs');
+    });
+
+    // PROFILE SECTION =========================
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile.ejs', {
+            user : req.user
+        });
+    });
+
+    // LOGOUT ==============================
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });*/
+
+// =============================================================================
+// AUTHENTICATE (FIRST LOGIN) ==================================================
+// =============================================================================
+
+    // locally --------------------------------
+        // LOGIN ===============================
+        // show the login form
+        app.get('/login', function(req, res) {
+            res.render('login.ejs', { message: req.flash('loginMessage') });
+        });
+
+        // process the login form
+        app.post('/login', passport.authenticate('local-login', {
+            successRedirect : '/pages/login', // redirect to the secure profile section
+            failureRedirect : '/', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+        }));
+
+        // SIGNUP =================================
+        // show the signup form
+        app.get('/signup', function(req, res) {
+            res.render('signup.ejs', { message: req.flash('signupMessage') });
+        });
+
+        // process the signup form
+        app.post('/signup', passport.authenticate('local-signup', {
+            successRedirect : '/pages/login', // redirect to the secure profile section
+            failureRedirect : '/pages/signup', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+        }));
+
+};
+
+
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    res.redirect('/');
+}
--- index.js
+++ index.js
@@ -2,11 +2,60 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var flash = require('connect-flash');
+var session = require('express-session');
+//var cookieSession = require('cookie-session');
 
 var app = express();
 
+app.use(cookieParser('rawr'));
+app.use(session({
+    secret: 'rawr',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash());
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+//app.use(session({secret: 'ssshhhhh'}));
+
+//var LocalStrategy = require('passport-local').Strategy;
+//require('./app/routes.js')(app, passport);
+
+// expose this function to our app using module.exports
+
+
+
+// need cookieParser middleware before we can do anything with cookies
+/*app.use(cookieParser());
+
+// set a cookie
+app.use(function (req, res, next) {
+  // check if client sent cookie
+  var cookie = req.cookies.cookieName;
+  if (cookie === undefined)
+  {
+    // no: set a new cookie
+    var randomNumber=Math.random().toString();
+    randomNumber=randomNumber.substring(2,randomNumber.length);
+    res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
+    console.log('cookie created successfully');
+  } 
+  else
+  {
+    // yes, cookie was already present 
+    console.log('cookie exists', cookie);
+  } 
+  next(); // <-- important!
+});*/
+
+
 
 var connection = mysql.createConnection({
 	host: 'animalshelter.cqfes6my9qk1.us-east-1.rds.amazonaws.com',
@@ -24,6 +73,8 @@ connection.connect(function(err){
 		console.log("Error connecting database D: \n");
 	}
 });
+
+connection.query('USE animalshelter');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -46,15 +97,27 @@ app.get('/about', function(request, response) {
 });
 
 app.get('/find', function(request, response) {
+	request.send(request);
 	response.render('pages/find');
 });
 
-app.get('/login', function(request, response) {
+/*app.get('/login', function(request, response) {
 	response.render('pages/login');
 });
 
+	app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/find', // redirect to the secure profile section
+        failureRedirect : '/', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));*/
+
 app.get('/signup', function(request, response) {
 	response.render('pages/signup');
+});
+
+app.get('/logout', function(request, response) {
+	request.logout();
+	response.redirect('/');
 });
 
 app.get('/adopt', function(request, response) {
@@ -66,12 +129,135 @@ app.get('/foster', function(request, response) {
 });
 
 app.post('/adopt_signup', function(request, response) {
-	console.log("SIGN UP WORKS");
+	console.log("form submitted");
+	//response.redirect('pages/find');
 	
-	//response.render('/signup');
-	//console.log(request.body.first_name);
+	var first_name			 = request.body.first_name;
+	var last_name			 = request.body.last_name;
+	var street				 = request.body.street;
+	var city				 = request.body.city;
+	var state				 = request.body.state;
+	var zip					 = request.body.zip;
+	var email				 = request.body.email;
+	var phone				 = request.body.phone;
+	var house_style			 = request.body.house_style;
+	var has_fenced_yard		 = request.body.has_fenced_yard;
+	var has_kids			 = request.body.has_kids;
+	var has_other_pets		 = request.body.has_other_pets;
+	var lifestyle_preference = request.body.lifestyle_preference;
+	var personality			 = request.body.personality;
+	var username			 = request.body.username;
+	var password			 = request.body.password;
 	
+	var q_size, q_kids, q_pets, q_personality, q_energy;
+	
+	if(has_fenced_yard==="on") {
+		has_fenced_yard = "TRUE";
+		q_size = "";
+	}
+	else {
+		has_fenced_yard = "FALSE";
+		q_size = "d.size<>'large'"; // no large dogs if they don't have a fenced yard
+	}
+	if(has_kids==="on") {
+		has_kids = "TRUE";
+		q_kids = " AND d.okay_with_kids='TRUE'"; // dog must be okay with kids
+	}
+	else {
+		has_kids = "FALSE";
+		q_kids = "";
+	}
+	if(has_other_pets==="on") {
+		has_other_pets = "TRUE";
+		q_pets = " AND d.okay_with_other_pets='TRUE'"; // dog with be okay with other pets
+	}
+	else {
+		has_other_pets = "FALSE";
+		q_pets = "";
+	}
+	
+	connection.query("SELECT * FROM animalshelter.Dog d", function(err, rows) {
+		if(rows===undefined) {
+			console.log("Empty");
+		}
+		response.render('pages/find', {r:rows});
+	});
+	
+	
+	
+	/*if(has_kids === "TRUE") {
+		if(has_other_pets === "TRUE") {
+			if(has_fenced_yard === "FALSE") {
+				connection.query('SELECT * FROM Dog WHERE okay_with_kids="TRUE" AND okay_with_pets="TRUE" AND size!="large"', function(err, rows, fields) {
+					response.render('pages/find', {r:rows});
+				});
+			}
+			else {
+				connection.query('SELECT * FROM Dog WHERE okay_with_kids="TRUE" AND okay_with_pets="TRUE"', function(err, rows, fields) {
+					response.render('pages/find', {r:rows});
+				});
+			}
+		}
+		
+		else {
+			if(has_fenced_yard === "FALSE") {
+				connection.query('SELECT * FROM Dog WHERE okay_with_kids="TRUE" AND size!="large"', function(err, rows, fields) {
+					response.render('pages/find', {r:rows});
+				});
+			}
+			else {
+				connection.query('SELECT * FROM Dog WHERE okay_with_kids="TRUE"', function(err, rows, fields) {
+					response.render('pages/find', {r:rows});
+				});
+			}
+		}
+	}
+	
+	else {
+		if(has_other_pets === "TRUE") {
+			if(has_fenced_yard === "FALSE") {
+				connection.query('SELECT * FROM Dog WHERE okay_with_pets="TRUE" AND size!="large"', function(err, rows, fields) {
+					response.render('pages/find', {r:rows});
+				});
+			}
+			else {
+				connection.query('SELECT * FROM Dog WHERE okay_with_pets="TRUE"', function(err, rows, fields) {
+					response.render('pages/find', {r:rows});
+				});
+			}
+		}
+		
+		else {
+			if(has_fenced_yard === "FALSE") {
+				connection.query('SELECT * FROM Dog WHEREAND size!="large"', function(err, rows, fields) {
+					response.render('pages/find', {r:rows});
+				});
+			}
+			else {
+				connection.query('SELECT * FROM Dog', function(err, rows, fields) {
+					response.render('pages/find', {r:rows});
+				});
+			}
+		}
+	}*/
+	
+	
+	
+	//var post  = {first_name: first_name, last_name: last_name, street: street, city: city, state: state, zip: zip, email: email, phone: phone, house_style, house_style, has_fenced_yard: has_fenced_yard, has_kids: has_kids, has_other_pets: has_other_pets, lifestyle_preference: lifestyle_preference, personality: personality, username: username, password: password};
 
+	var post  = {first_name: first_name, last_name: last_name, street: street, city: city, state: state, zip: zip, email: email, phone: phone, house_style, house_style, has_fenced_yard: has_fenced_yard, has_kids: has_kids, has_other_pets: has_other_pets, lifestyle_preference: lifestyle_preference, personality: personality, username: username, password: password};
+	
+	/*var query = connection.query('INSERT INTO Adopter SET ?', post, function(err, result) {
+		if(err) throw err;
+		//connection.end();
+		
+		
+		
+		var query = connection.query('SELECT * FROM Dog WHERE ', function(err, rows, fields) {
+			response.render('pages/find', {r:rows});
+		});
+	});
+	console.log(query.sql);*/
 });
 
 app.post('/', function(req, res) {
