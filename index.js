c744788f5deef80bcc06210a381fae3dce7a0793
swishypants <shamuflute@gmail.com>
2018-03-18 13:13:48 -0700

    migrating to postgres

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
 var mysql = require('mysql');
+const { Client } = require('pg');
 
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var connection = new Client({
+	connectionString: process.env.DATABASE_URL,
+});
+/*
 var connection = mysql.createConnection({
 	host: process.env.host,
 	port: process.env.port,
@@ -11,6 +20,7 @@ var connection = mysql.createConnection({
 	password: process.env.password,
 	database: process.env.database
 });
+*/
 
 connection.connect(function(err){
 	if(!err){
@@ -21,7 +31,7 @@ connection.connect(function(err){
 	}
 });
 
-connection.query('USE animalshelter');
+//connection.query('USE animalshelter');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -47,117 +57,117 @@ var dog_query = "SELECT * FROM Dog d";
 var shelter_query = "SELECT * FROM Shelter s";
 
 app.get('/dog', function(request, response) {
-	connection.query(dog_query, function(err, rows) {
-		response.render('pages/dog', {r: rows});
+	connection.query(dog_query, function(err, result) {
+		response.render('pages/dog', {r: result.rows});
 	});
 	
 	// sort
 	app.get('/sort-id', function(request, response) {
-		connection.query(dog_query + " ORDER BY d.dog_id ASC", function(err, rows) {
-			response.render('pages/dog', {r:rows});
+		connection.query(dog_query + " ORDER BY d.dog_id ASC", function(err, result) {
+			response.render('pages/dog', {r:result.rows});
 		});
 	});
 	
 	app.get('/dog/sort-name', function(request, response) {
-		connection.query(dog_query + " ORDER BY d.name ASC", function(err, rows) {
-			response.render('pages/dog', {r:rows});
+		connection.query(dog_query + " ORDER BY d.name ASC", function(err, result) {
+			response.render('pages/dog', {r:result.rows});
 		});
 	});
 	
 	app.get('/dog/sort-age', function(request, response) {
-		connection.query(dog_query + " ORDER BY d.age ASC", function(err, rows) {
-			response.render('pages/dog', {r:rows});
+		connection.query(dog_query + " ORDER BY d.age ASC", function(err, result) {
+			response.render('pages/dog', {r:result.rows});
 		});
 	});
 	
 	app.get('/dog/sort-gender', function(request, response) {
-		connection.query(dog_query + " ORDER BY d.gender ASC", function(err, rows) {
-			response.render('pages/dog', {r:rows});
+		connection.query(dog_query + " ORDER BY d.gender ASC", function(err, result) {
+			response.render('pages/dog', {r:result.rows});
 		});
 	});
 	
 	app.get('/dog/sort-breed', function(request, response) {
-		connection.query(dog_query + " ORDER BY d.breed ASC", function(err, rows) {
-			response.render('pages/dog', {r:rows});
+		connection.query(dog_query + " ORDER BY d.breed ASC", function(err, result) {
+			response.render('pages/dog', {r:result.rows});
 		});
 	});
 	
 	app.get('/dog/sort-size', function(request, response) {
-		connection.query(dog_query + " ORDER BY d.size DESC", function(err, rows) {
-			response.render('pages/dog', {r:rows});
+		connection.query(dog_query + " ORDER BY d.size DESC", function(err, result) {
+			response.render('pages/dog', {r:result.rows});
 		});
 	});
 	
 	app.get('/dog/sort-coat', function(request, response) {
-		connection.query(dog_query + " ORDER BY d.coat ASC", function(err, rows) {
-			response.render('pages/dog', {r:rows});
+		connection.query(dog_query + " ORDER BY d.coat ASC", function(err, result) {
+			response.render('pages/dog', {r:result.rows});
 		});
 	});
 	
 	app.get('/dog/sort-personality', function(request, response) {
-		connection.query(dog_query + " ORDER BY d.personality ASC", function(err, rows) {
-			response.render('pages/dog', {r:rows});
+		connection.query(dog_query + " ORDER BY d.personality ASC", function(err, result) {
+			response.render('pages/dog', {r:result.rows});
 		});
 	});
 	
 	app.get('/dog/sort-energy', function(request, response) {
-		connection.query(dog_query + " ORDER BY CASE WHEN d.energy_level = 'low' THEN 1 WHEN d.energy_level = 'medium' THEN 2 WHEN d.energy_level = 'high' THEN 3 ELSE 4 END", function(err, rows) {
-			response.render('pages/dog', {r:rows});
+		connection.query(dog_query + " ORDER BY CASE WHEN d.energy_level = 'low' THEN 1 WHEN d.energy_level = 'medium' THEN 2 WHEN d.energy_level = 'high' THEN 3 ELSE 4 END", function(err, result) {
+			response.render('pages/dog', {r:result.rows});
 		});
 	});
 	
 	app.get('/dog/sort-shelter', function(request, response) {
-		connection.query(dog_query + " ORDER BY d.shelter_id ASC", function(err, rows) {
-			response.render('pages/dog', {r:rows});
+		connection.query(dog_query + " ORDER BY d.shelter_id ASC", function(err, result) {
+			response.render('pages/dog', {r:result.rows});
 		});
 	});
 });
 
 app.get('/shelter', function(request, response) {
-	connection.query(shelter_query, function(err, rows) {
-		response.render('pages/shelter', {r:rows});
+	connection.query(shelter_query, function(err, result) {
+		response.render('pages/shelter', {r:result.rows});
 	});
 	
 	// sort
-	app.get('/shelter/sort-id', function(request, response) {
-		connection.query(shelter_query + " ORDER BY s.shelter_id ASC", function(err, rows) {
-			response.render('pages/shelter', {r:rows});
+	app.get('/shelter/sort-id', function(request, result) {
+		connection.query(shelter_query + " ORDER BY s.shelter_id ASC", function(err, result) {
+			response.render('pages/shelter', {r:result.rows});
 		});
 	});
 	
 	app.get('/shelter/sort-name', function(request, response) {
-		connection.query(shelter_query + " ORDER BY s.shelter_name ASC", function(err, rows) {
-			response.render('pages/shelter', {r:rows});
+		connection.query(shelter_query + " ORDER BY s.shelter_name ASC", function(err, result) {
+			response.render('pages/shelter', {r:result.rows});
 		});
 	});
 	
 	app.get('/shelter/sort-street', function(request, response) {
-		connection.query(shelter_query + " ORDER BY s.street ASC", function(err, rows) {
-			response.render('pages/shelter', {r:rows});
+		connection.query(shelter_query + " ORDER BY s.street ASC", function(err, result) {
+			response.render('pages/shelter', {r:result.rows});
 		});
 	});
 	
 	app.get('/shelter/sort-city', function(request, response) {
-		connection.query(shelter_query + " ORDER BY s.city ASC", function(err, rows) {
-			response.render('pages/shelter', {r:rows});
+		connection.query(shelter_query + " ORDER BY s.city ASC", function(err, result) {
+			response.render('pages/shelter', {r:result.rows});
 		});
 	});
 	
 	app.get('/shelter/sort-state', function(request, response) {
-		connection.query(shelter_query + " ORDER BY s.state ASC", function(err, rows) {
-			response.render('pages/shelter', {r:rows});
+		connection.query(shelter_query + " ORDER BY s.state ASC", function(err, result) {
+			response.render('pages/shelter', {r:result.rows});
 		});
 	});
 	
 	app.get('/shelter/sort-zip', function(request, response) {
-		connection.query(shelter_query + " ORDER BY s.zip ASC", function(err, rows) {
-			response.render('pages/shelter', {r:rows});
+		connection.query(shelter_query + " ORDER BY s.zip ASC", function(err, result) {
+			response.render('pages/shelter', {r:result.rows});
 		});
 	});
 	
 	app.get('/shelter/sort-phone', function(request, response) {
-		connection.query(shelter_query + " ORDER BY s.phone ASC", function(err, rows) {
-			response.render('pages/shelter', {r:rows});
+		connection.query(shelter_query + " ORDER BY s.phone ASC", function(err, result) {
+			response.render('pages/shelter', {r:result.rows});
 		});
 	});
 });
@@ -165,83 +175,76 @@ app.get('/shelter', function(request, response) {
 app.get('/shelter_dogs/:shelter_id', function(request, response) {
 	var id = request.params.shelter_id;
 	var shelter_id_query = "SELECT * FROM Shelter s, Dog d WHERE s.shelter_id=d.shelter_id AND s.shelter_id = '" + id + "'";
-	connection.query(shelter_id_query, function(err, rows) {
-		response.render('pages/shelter_dogs', {r:rows});
+	connection.query(shelter_id_query, function(err, result) {
+		response.render('pages/shelter_dogs', {r:result.rows});
 	});
 	
 	// sort
 	app.get('/shelter_dogs/' + id + '/sort-id', function(request, response) {
-		connection.query(shelter_id_query + " ORDER BY d.dog_id ASC", function(err, rows) {
-			response.render('pages/shelter_dogs', {r:rows});
+		connection.query(shelter_id_query + " ORDER BY d.dog_id ASC", function(err, result) {
+			response.render('pages/shelter_dogs', {r:result.rows});
 		});
 	});
 	
 	app.get('/shelter_dogs/' + id + '/sort-name', function(request, response) {
-		connection.query(shelter_id_query + " ORDER BY d.name ASC", function(err, rows) {
-			response.render('pages/shelter_dogs', {r:rows});
+		connection.query(shelter_id_query + " ORDER BY d.name ASC", function(err, result) {
+			response.render('pages/shelter_dogs', {r:result.rows});
 		});
 	});
 	
 	app.get('/shelter_dogs/' + id + '/sort-age', function(request, response) {
-		connection.query(shelter_id_query + " ORDER BY d.age ASC", function(err, rows) {
-			response.render('pages/shelter_dogs', {r:rows});
+		connection.query(shelter_id_query + " ORDER BY d.age ASC", function(err, result) {
+			response.render('pages/shelter_dogs', {r:result.rows});
 		});
 	});
 	
 	app.get('/shelter_dogs/' + id + '/sort-gender', function(request, response) {
-		connection.query(shelter_id_query + " ORDER BY d.gender ASC", function(err, rows) {
-			response.render('pages/shelter_dogs', {r:rows});
+		connection.query(shelter_id_query + " ORDER BY d.gender ASC", function(err, result) {
+			response.render('pages/shelter_dogs', {r:result.rows});
 		});
 	});
 	
 	app.get('/shelter_dogs/' + id + '/sort-breed', function(request, response) {
-		connection.query(shelter_id_query + " ORDER BY d.breed ASC", function(err, rows) {
-			response.render('pages/shelter_dogs', {r:rows});
+		connection.query(shelter_id_query + " ORDER BY d.breed ASC", function(err, result) {
+			response.render('pages/shelter_dogs', {r:result.rows});
 		});
 	});
 	
 	app.get('/shelter_dogs/' + id + '/sort-size', function(request, response) {
-		connection.query(shelter_id_query + " ORDER BY d.size DESC", function(err, rows) {
-			response.render('pages/shelter_dogs', {r:rows});
+		connection.query(shelter_id_query + " ORDER BY d.size DESC", function(err, result) {
+			response.render('pages/shelter_dogs', {r:result.rows});
 		});
 	});
 	
 	app.get('/shelter_dogs/' + id + '/sort-coat', function(request, response) {
-		connection.query(shelter_id_query + " ORDER BY d.coat ASC", function(err, rows) {
-			response.render('pages/shelter_dogs', {r:rows});
+		connection.query(shelter_id_query + " ORDER BY d.coat ASC", function(err, result) {
+			response.render('pages/shelter_dogs', {r:result.rows});
 		});
 	});
 	
 	app.get('/shelter_dogs/' + id + '/sort-personality', function(request, response) {
-		connection.query(shelter_id_query + " ORDER BY d.personality ASC", function(err, rows) {
-			response.render('pages/shelter_dogs', {r:rows});
+		connection.query(shelter_id_query + " ORDER BY d.personality ASC", function(err, result) {
+			response.render('pages/shelter_dogs', {r:result.rows});
 		});
 	});
 	
 	app.get('/shelter_dogs/' + id + '/sort-energy', function(request, response) {
-		connection.query(shelter_id_query + " ORDER BY CASE WHEN d.energy_level = 'low' THEN 1 WHEN d.energy_level = 'medium' THEN 2 WHEN d.energy_level = 'high' THEN 3 ELSE 4 END", function(err, rows) {
-			response.render('pages/shelter_dogs', {r:rows});
+		connection.query(shelter_id_query + " ORDER BY CASE WHEN d.energy_level = 'low' THEN 1 WHEN d.energy_level = 'medium' THEN 2 WHEN d.energy_level = 'high' THEN 3 ELSE 4 END", function(err, result) {
+			response.render('pages/shelter_dogs', {r:result.rows});
 		});
 	});
 	
 	app.get('/shelter_dogs/' + id + '/sort-shelter', function(request, response) {
-		connection.query(shelter_id_query + " ORDER BY d.shelter_id ASC", function(err, rows) {
-			response.render('pages/shelter_dogs', {r:rows});
+		connection.query(shelter_id_query + " ORDER BY d.shelter_id ASC", function(err, result) {
+			response.render('pages/shelter_dogs', {r:result.rows});
 		});
 	});
 });
 
 app.post('/find', function(request, response) {
 	console.log("form submitted");
+	console.log(request)
 	
-	var first_name			 = request.body.first_name;
-	var last_name			 = request.body.last_name;
-	var street				 = request.body.street;
-	var city				 = request.body.city;
-	var state				 = request.body.state;
-	var zip					 = request.body.zip;
-	var email				 = request.body.email;
-	var phone				 = request.body.phone;
 	var house_style			 = request.body.house_style;
 	var has_fenced_yard		 = request.body.has_fenced_yard;
 	var has_kids			 = request.body.has_kids;
@@ -315,8 +318,8 @@ app.post('/find', function(request, response) {
 		q_energy = "";
 	}
 	
-	connection.query(q, function(err, rows) {
-		response.render('pages/find', {r:rows});
+	connection.query(q, function(err, result) {
+		response.render('pages/find', {r:result.rows});
 	});
 	
 	/*var post  = {first_name: first_name, last_name: last_name, street: street, city: city, state: state, zip: zip, email: email, phone: phone, house_style, house_style, has_fenced_yard: has_fenced_yard, has_kids: has_kids, has_other_pets: has_other_pets, lifestyle_preference: lifestyle_preference, personality: personality};
@@ -327,70 +330,70 @@ app.post('/find', function(request, response) {
 		
 		
 		
-		var query = connection.query('SELECT * FROM Dog WHERE ', function(err, rows, fields) {
-			response.render('pages/find', {r:rows});
+		var query = connection.query('SELECT * FROM Dog WHERE ', function(err, result.rows, fields) {
+			response.render('pages/find', {r:result.rows});
 		});
 	});
 	console.log(query.sql);*/
 	
 	// sort find
 	app.get('/find/sort-id', function(request, response) {
-		connection.query(q + " ORDER BY d.dog_id ASC", function(err, rows) {
-			response.render('pages/find', {r:rows});
+		connection.query(q + " ORDER BY d.dog_id ASC", function(err, result) {
+			response.render('pages/find', {r:result.rows});
 		});
 	});
 
 	app.get('/find/sort-name', function(request, response) {
-		connection.query(q + " ORDER BY d.name ASC", function(err, rows) {
-			response.render('pages/find', {r:rows});
+		connection.query(q + " ORDER BY d.name ASC", function(err, result) {
+			response.render('pages/find', {r:result.rows});
 		});
 	});
 
 	app.get('/find/sort-age', function(request, response) {
-		connection.query(q + " ORDER BY d.age ASC", function(err, rows) {
-			response.render('pages/find', {r:rows});
+		connection.query(q + " ORDER BY d.age ASC", function(err, result) {
+			response.render('pages/find', {r:result.rows});
 		});
 	});
 
 	app.get('/find/sort-gender', function(request, response) {
-		connection.query(q + " ORDER BY d.gender ASC", function(err, rows) {
-			response.render('pages/find', {r:rows});
+		connection.query(q + " ORDER BY d.gender ASC", function(err, result) {
+			response.render('pages/find', {r:result.rows});
 		});
 	});
 
 	app.get('/find/sort-breed', function(request, response) {
-		connection.query(q + " ORDER BY d.breed ASC", function(err, rows) {
-			response.render('pages/find', {r:rows});
+		connection.query(q + " ORDER BY d.breed ASC", function(err, result) {
+			response.render('pages/find', {r:result.rows});
 		});
 	});
 
 	app.get('/find/sort-size', function(request, response) {
-		connection.query(q + " ORDER BY d.size DESC", function(err, rows) {
-			response.render('pages/find', {r:rows});
+		connection.query(q + " ORDER BY d.size DESC", function(err, result) {
+			response.render('pages/find', {r:result.rows});
 		});
 	});
 
 	app.get('/find/sort-coat', function(request, response) {
-		connection.query(q + " ORDER BY d.coat ASC", function(err, rows) {
-			response.render('pages/find', {r:rows});
+		connection.query(q + " ORDER BY d.coat ASC", function(err, result) {
+			response.render('pages/find', {r:result.rows});
 		});
 	});
 
 	app.get('/find/sort-personality', function(request, response) {
-		connection.query(q + " ORDER BY d.personality ASC", function(err, rows) {
-			response.render('pages/find', {r:rows});
+		connection.query(q + " ORDER BY d.personality ASC", function(err, result) {
+			response.render('pages/find', {r:result.rows});
 		});
 	});
 
 	app.get('/find/sort-energy', function(request, response) {
-		connection.query(q + " ORDER BY CASE WHEN d.energy_level = 'low' THEN 1 WHEN d.energy_level = 'medium' THEN 2 WHEN d.energy_level = 'high' THEN 3 ELSE 4 END", function(err, rows) {
-			response.render('pages/find', {r:rows});
+		connection.query(q + " ORDER BY CASE WHEN d.energy_level = 'low' THEN 1 WHEN d.energy_level = 'medium' THEN 2 WHEN d.energy_level = 'high' THEN 3 ELSE 4 END", function(err, result) {
+			response.render('pages/find', {r:result.rows});
 		});
 	});
 
 	app.get('/find/sort-shelter', function(request, response) {
-		connection.query(q + " ORDER BY d.shelter_id ASC", function(err, rows) {
-			response.render('pages/find', {r:rows});
+		connection.query(q + " ORDER BY d.shelter_id ASC", function(err, result) {
+			response.render('pages/find', {r:result.rows});
 		});
 	});
 });
